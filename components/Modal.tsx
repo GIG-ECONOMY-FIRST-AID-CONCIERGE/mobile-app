d6a59e1f046a6ad6f08c4ed72ca815216c2d0b26
SrVieira <matheusvieirasto@gmail.com>
2023-05-19 15:29:55 -0300

    add typescript dependencies

---
+++ components/Modal.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Modal = (): JSX.Element => {
+    return (
+        <div>
+            teste
+            <div>teste 2</div>
+        </div>
+    );
+};
+
+export default Modal;

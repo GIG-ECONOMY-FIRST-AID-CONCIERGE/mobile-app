f457db106c90b9c051369bbe26da29b45bbecd2b
SrVieira <matheusvieirasto@gmail.com>
2023-05-22 12:48:29 -0300

    update service card

--- components/Modal/screens/ModalServices.tsx
+++ components/Modal/screens/ModalServices.tsx
@@ -1,5 +1,10 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Image } from 'react-native';
+
+// ASSETS
+import ambulanceIcon from '../../../assets/ambulance.png';
+import truckIcon from '../../../assets/towtruck.png';
+import completIcon from '../../../assets/complete.png';
 
 // STYLES
 import styles from './styles';
@@ -8,14 +13,23 @@ const ModalServices = (): JSX.Element => {
     return (
         <View style={styles.container}>
             <View style={styles.serviceCard}>
+                <View style={styles.serviceIcon}>
+                    <Image source={ambulanceIcon} style={styles.icon} />
+                </View>    
                 <Text style={styles.serviceTitle}>Assistência</Text>
                 <Text style={styles.serviceSubtitle}>Médica</Text>
             </View>
             <View style={styles.serviceCard}>
+                <View style={styles.serviceIcon}>
+                    <Image source={truckIcon} style={styles.icon} />
+                </View>
                 <Text style={styles.serviceTitle}>Assistência</Text>
                 <Text style={styles.serviceSubtitle}>Guincho</Text>
             </View>       
             <View style={styles.serviceCard}>
+                <View style={styles.serviceIcon}>
+                    <Image source={completIcon} style={styles.iconLarge} />
+                </View>
                 <Text style={styles.serviceTitle}>Solicitar</Text>
                 <Text style={styles.serviceSubtitle}>Ambos</Text>
             </View>                    
--- components/Modal/screens/styles.ts
+++ components/Modal/screens/styles.ts
@@ -9,7 +9,7 @@ const styles = StyleSheet.create({
   },
   serviceCard: {
     width: '32%',
-    paddingVertical: 35,
+    paddingVertical: 24,
     borderWidth: 1,
     borderColor: '#D6D6D6',
     borderRadius: 14
@@ -21,6 +21,19 @@ const styles = StyleSheet.create({
   serviceSubtitle: {
     color: '#787878',
     textAlign: 'center',
+  },
+  serviceIcon: {
+    marginBottom: 4,
+    flexDirection: 'row',
+    justifyContent: 'center',
+  },
+  icon: {
+    width: 35,
+    height: 26,
+  },
+  iconLarge: {
+    width: 65,
+    height: 22,
   }
 });
 
